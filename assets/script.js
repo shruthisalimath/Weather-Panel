b19ba1ef39ffaa9b0628c81601ab180ab3773f04
shru <shruthishekar@gmail.com>
2023-04-04 17:22:25 -0600

    weather forcast for 5 days

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ var currentTempEl = $("#tempResult");
 var currentHumidityEl = $("#humidityResult");
 var currentWindEl = $("#windResult");
 
-var forcastEl = $("#forecastContaine");
+var forcastEl = $("#forecastContainer");
 
 const date =new Date();
 var dateString = date.toLocaleDateString();
@@ -31,10 +31,10 @@ var cityArray = JSON.parse(localStorage.getItem("savedCities")) || [];
 
 
 
-var getCurrentWeather = function(inputCityEl)
+var getCurrentWeather = function(inputCity)
 {
     //var city = $("#currentCity");
-    const requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCityEl + "&appid=" + myApiKey;
+    const requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + myApiKey;
     $.ajax({
         url: requestUrl,
         method: 'GET',
@@ -42,20 +42,68 @@ var getCurrentWeather = function(inputCityEl)
     {
         console.log(response);
         var cityInfo = response.name;
-        var temp = response.main.temp;
+        //get temperature and cover it to fahrenheit
+        let temp = (response.main.temp - 273.15) * 1.80 + 32;
+        let tempF = Math.floor(temp);
+
+        //var temp = response.main.temp;
         var humidity = response.main.humidity;
         var wind = response.wind.speed;
         //var lat = response.coord.lat;
        // var lon = response .coord.lon;
         var icon = response.weather[0].icon;
-        var iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+       // var iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         currentCityEl.text(cityInfo + "(" + dateString + ")"); 
         currentCityEl.append("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' />" );
-        currentTempEl.text("Temperature: " + temp + "°F");
-        currentHumidityEl.text("Humidity: " + humidity + "%");
+        currentTempEl.text("Temperature: " + tempF + "°F");
         currentWindEl.text("Wind: " + wind + "MPH");
-    })
+        currentHumidityEl.text("Humidity: " + humidity + "%");
+        
+    });
 }
+
+var getForecast = function(inputCityEl)
+{
+    //var lat = response.coord.lat;
+    //var lon = response.coord.lon;
+    //const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + myApiKey + "&lat=" + lat +  "&lon=" + lon;
+    const foreCastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + myApiKey;
+   
+    $.ajax({
+        url: foreCastUrl,
+        method: "GET",
+    }).then(function(response)
+    {
+        console.log(response);
+       
+        $("#forecast").empty();
+        //var to hold response.list
+        var result = response.list;
+        console.log(result);
+        for (var i = 0; i < result.length; i++);
+        {
+            var foreCastDate = moment(result[i].dt_txt).format("L");
+            console.log(foreCastDate);
+            //get temperature and cover it to fahrenheit
+            let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+            let tempF = Math.floor(temp);
+
+            var card = $("<div>").addClass("card col-md-2 text-white");
+            var cardBody = $("<div>").addClass("card-body p-3 foreCastBody");
+            var cityDate = $("<h3>").addClass("card-title").text(foreCastDate);
+            var cityTemp = $("<p>").addClass("card-text foreCastTemp").text("Temperature : " + tempF + "°F");
+            var cityWind = $("<p>").addClass("card-text foreCastWind").text("Wind : " + result[i].main.wind + "MPH");
+            var cityHumidity = $("<p>").addClass("card-text foreCastHumid").text("Humidity : " + result[i].main.humidity + "%");
+            var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + result[i].weather[0].icon + ".png");
+
+            cardBody.append(cityDate, image, cityTemp, cityWind, cityHumidity);
+            card.append(cardBody);
+            $("#forecast").append(card);
+        }
+
+    });
+}
+
 
 
 
@@ -69,7 +117,7 @@ $("#searchBtn").on("click",function (event) {
     else {
         var inputCityEl =$("#inputCity").val();
         getCurrentWeather(inputCityEl);
-       // getForecast(inputCityEl);
+        getForecast(inputCityEl);
         $("#inputCity").val("");
          //$("#forecastContainer").addClass('show');
     }
